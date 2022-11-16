238f79e3816f724c08b51da336a8aacb201ce786
Bichchuran10 <bichchuran10@gmail.com>
2022-11-17 02:16:04 +0530

    added currying in the end

--- index.js
+++ index.js
@@ -111,3 +111,29 @@ let bound2= printAge.bind(student)
 
 
 console.log(bound2())
+
+
+//currying in JS
+
+//using bind method
+
+let multiply=function(x,y)
+{
+    console.log(x*y)
+}
+
+let multiplyByTwo= multiply.bind(this,2) //2 as x
+multiplyByTwo(4) //4 as y
+
+
+//using closures
+
+let add=function(a)
+{
+    return function(b){
+        console.log(a+b)
+    }
+}
+
+let addTwoNumbers=add(2) //a as 2
+addTwoNumbers(3) //b as 3
